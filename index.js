5e1410e95d64ed090b8d6f6974b5251eff9d37b6
Almas <aimanbaev6@gmail.com>
2022-12-03 12:42:22 +0600

    make adaptive header, make slider

---
+++ index.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	navbar.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
+}
+
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
