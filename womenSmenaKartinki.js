193e52a5b679e3180d2926011a33ceed805bfbdb
Almas <aimanbaev6@gmail.com>
2022-12-20 00:42:42 +0600

    Women page

---
+++ womenSmenaKartinki.js
@@ -0,0 +1,70 @@
+function newkurtka1(e){
+    e.src = "img/sary-kiz-kurtka-model.png"
+}
+function oldkurtka1(e){
+    e.src = "img/sary-kiz-kurtka.png"
+}
+function newkurtka2(e){
+	e.src="img/black-kiz-kurtka-model.png";
+}
+function oldkurtka2(e){
+    e.src = "img/black-kiz-kurtka.png";
+}
+function newkurtka3(e){
+	e.src="img/blue-kiz-kurtka-model.png";
+}
+function oldkurtka3(e){
+    e.src = "img/blue-kiz-kurtka.png";
+}
+
+
+function newFutbolka1(e){
+	e.src="img/1465Black-futbolka-model.png";
+}
+function oldFutbolka1(e){
+    e.src = "img/1465Black-futbolka.png";
+}
+function newFutbolka2(e){
+	e.src="img/1465Brown-futbolka-model.png";
+}
+function oldFutbolka2(e){
+    e.src = "img/1465Brown-futbolka.png";
+}
+function newFutbolka3(e){
+	e.src="img/KopSoz-futbolka-model.png";
+}
+function oldFutbolka3(e){
+    e.src = "img/KopSoz-futbolka.png";
+}
+function newFutbolka4(e){
+	e.src="img/baryZhaksyBolady-futbolka-model.png";
+}
+function oldFutbolka4(e){
+    e.src = "img/baryZhaksyBolady-futbolka.png";
+}
+
+
+function newHudi1(e){
+	e.src="img/kok-rubashka-model.png";
+}
+function oldHudi1(e){
+    e.src = "img/kok-rubashka.png";
+}
+function newHudi2(e){
+	e.src="img/zhasyl-sportak-model.png";
+}
+function oldHudi2(e){
+    e.src = "img/zhasyl-sportak.png";
+}
+function newHudi3(e){
+	e.src="img/brown-sportak-model.png";
+}
+function oldHudi3(e){
+    e.src = "img/brown-sportak.png";
+}
+function newHudi4(e){
+	e.src="img/kok-switshot-model.png";
+}
+function oldHudi4(e){
+    e.src = "img/kok-switshot.png";
+}
