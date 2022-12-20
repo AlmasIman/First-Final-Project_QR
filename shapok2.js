3c0d67e5244f2af788c172bd58d16a955f1a9847
shapok <210107108@stu.sdu.edu.kz>
2022-12-20 07:40:29 +0300

    make page Collection

--- shapok2.js
+++ shapok2.js
@@ -1,11 +1,31 @@
-let myImage = document.querySelector('.katalog-photo-2');
+// let myImage = document.querySelector('.katalog-photo-2');
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === "katalog-photo-2.png"){
-        myImage.setAttribute('src', "img/katalog-photo-1.png");
-    }
-    else {
-        myImage.setAttribute('src', "img/katalog-photo-2.png");
-    }
-}
+// myImage.onclick = function() {
+//     let mySrc = myImage.getAttribute('src');
+//     if(mySrc === "katalog-photo-2.png"){
+//         myImage.setAttribute('src', "img/katalog-photo-1.png");
+//     }
+//     else {
+//         myImage.setAttribute('src', "img/katalog-photo-2.png");
+//     }
+// }
+
+
+const faq = document.querySelectorAll(".faqs .faq-header");
+
+faq.forEach((header,i) => {
+    header.addEventListener("click", () => {
+        header.nextElementSibling.classList.toggle("active");
+
+        const open = header.querySelector(".open");
+        const close = header.querySelector(".close");
+
+        if(header.nextElementSibling.classList.contains("active")){
+                open.classList.remove("active");
+                close.classList.add("active");
+            }else{
+                open.classList.add("active");
+                close.classList.remove("active");
+            }
+    });
+});
